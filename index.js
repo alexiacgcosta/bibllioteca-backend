ce400cd7d26ca5b9ef93d10f46e7db76bb6e5987
alexiacgcosta <alexia.cassia09@gmail.com>
2023-10-13 13:43:50 -0400

    updating index

--- index.js
+++ index.js
@@ -4,18 +4,15 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
-const swaggerFile = require('./swagger/swagger_output.json');
-const swaggerOptions = { customCssUrl: './public/swagger-ui.css'};
+const swaggerOptions = { customCssUrl: '/swagger-ui.css' };
 const routes = require('./src/routes');
-
-
 const authDocProducao = require('./src/middlewares/authDoc');
 const app = express();
 require('dotenv').config();
 
-//configuração do express
+
+
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,26 +20,22 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-//rotas do swagger
-routes(app);
 
-
-//doc do swagger
-if (process.env.NODE_ENV !== 'test') {
-  const swaggerFile = require('./swagger/swagger_output.json');
-  app.get('/', (req, res) => { /* #swagger.ignore = true */ res.redirect('/doc'); });
-  app.use('/doc', authDocProducao, swaggerUi.serve, swaggerUi.setup(swaggerFile, swaggerOptions));
+if(process.env.NODE_ENV !== 'test'){
+    const swaggerFile = require('./swagger/swagger_output.json');
+    app.get('/', (req, res) => { /* #swagger.ignore = true */ res.redirect('/doc'); });
+    app.use('/doc', authDocProducao, swaggerUi.serve, swaggerUi.setup(swaggerFile, swaggerOptions));
 }
 
 
 
+routes(app);
 
 
-//inicialização do servidor
+
 if (process.env.NODE_ENV !== 'test') {
-  
-  const PORT = process.env.PORT || 4000;
-  app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
+    const PORT = process.env.PORT || 4000;
+    app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
 }
 
 module.exports = app;
